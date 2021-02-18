e98367d8531ca38c8dea9854a8801efe3e3eb5f9
Junior Medina <juniiormediina@gmail.com>
2021-02-18 12:09:13 -0500

    updated

--- frontend/src/assets/Data/projects_data.js
+++ frontend/src/assets/Data/projects_data.js
@@ -2,6 +2,7 @@ import Podcast from "../projects_images/Podcast.svg";
 import Gifos from "../projects_images/Gifos.svg";
 import Delilah from "../projects_images/Delilah.svg";
 import Warehouse from "../projects_images/Warehouse.svg";
+import ChatReact from "../projects_images/ChatReact.svg";
 
 const data_projects = [
   {
@@ -34,6 +35,13 @@ const data_projects = [
     deployed_url: "https://github.com/juniiormediina/DataWarehouse",
     github_url: "https://github.com/juniiormediina/DataWarehouse",
     category: ["MySQL", "react", "node.js"],
+  },
+  {
+    name: "Chat React",
+    image: ChatReact,
+    deployed_url: "https://react-chat-jm.herokuapp.com/",
+    github_url: "https://github.com/juniiormediina/Chat-React",
+    category: ["MySQL", "react.js", "node.js"],
   },
 ];
 
