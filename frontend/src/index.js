ebb82dca351ce51cd026509d8c5df53338792b27
Junior Medina <juniiormediina@gmail.com>
2021-03-19 11:22:50 -0500

    Resume updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-/* import "./index.css"; */
-import './styles/styles.css';
+import "./styles/styles.css";
 import App from "./App";
 
 ReactDOM.render(
