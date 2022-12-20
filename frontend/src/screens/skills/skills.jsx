c95ee13c6affffb2978baf8ad61ef8aee58cbd1c
Junior Humberto Medina Bustamante <juniiormediina@gmail.com>
2022-12-20 18:39:39 -0500

    refactoring to screen for skills

--- frontend/src/screens/skills/skills.jsx
+++ frontend/src/screens/skills/skills.jsx
@@ -1,17 +1,146 @@
-import React from 'react';
-import Backend from './backend';
-import Frontend from './frontend';
+import React, { useState } from 'react';
 import './skills.css';
 
 const Skills = () => {
+  const [frontend] = useState([
+    { id: 1, skillName: 'HTML', level: 'Advanced' },
+    {
+      id: 2,
+      skillName: 'CSS',
+      level: 'Advanced',
+    },
+    {
+      id: 3,
+      skillName: 'JavaScript',
+      level: 'Advanced',
+    },
+    {
+      id: 4,
+      skillName: 'Angular',
+      level: 'Intermediate',
+    },
+    {
+      id: 5,
+      skillName: 'React',
+      level: 'Intermediate',
+    },
+    {
+      id: 6,
+      skillName: 'Git',
+      level: 'Advanced',
+    },
+  ]);
+
+  const [backend] = useState([
+    { id: 1, skillName: 'JAVA', level: 'Intermediate' },
+    {
+      id: 2,
+      skillName: 'Spring Boot',
+      level: 'Intermediate',
+    },
+    {
+      id: 3,
+      skillName: 'Node Js',
+      level: 'Intermediate',
+    },
+    {
+      id: 4,
+      skillName: 'Data Base',
+      level: 'Intermediate',
+    },
+    {
+      id: 5,
+      skillName: 'AWS',
+      level: 'Intermediate',
+    },
+    {
+      id: 6,
+      skillName: 'DevOps',
+      level: 'Advanced',
+    },
+  ]);
+
   return (
     <section className='skills section' id='skills'>
       <h2 className='section__title'>Skills</h2>
       <span className='section__subtitle'>My technical level</span>
 
       <div className='skills__container container grid'>
-        <Frontend />
-        <Backend />
+        {/* Frontend */}
+
+        <div className='skills__content'>
+          <h3 className='skills__title'>Frontend developer</h3>
+
+          <div className='skills__box'>
+            <div className='skills__group'>
+              {frontend.map((frontend) =>
+                frontend.id <= 3 ? (
+                  <div className='skills__data' key={frontend.id}>
+                    <i className='bx bx-badge-check'></i>
+
+                    <div>
+                      <h3 className='skills__name'>{frontend.skillName}</h3>
+                      <span className='skills_level'>{frontend.level}</span>
+                    </div>
+                  </div>
+                ) : null
+              )}
+            </div>
+
+            <div className='skills__group'>
+              {frontend.map((frontend) =>
+                frontend.id >= 4 ? (
+                  <div className='skills__data' key={frontend.id}>
+                    <i className='bx bx-badge-check'></i>
+
+                    <div>
+                      <h3 className='skills__name'>{frontend.skillName}</h3>
+                      <span className='skills_level'>{frontend.level}</span>
+                    </div>
+                  </div>
+                ) : null
+              )}
+            </div>
+          </div>
+        </div>
+
+        {/* Backend */}
+
+        <div className='skills__content'>
+          <h3 className='skills__title'>Backend Developer</h3>
+
+          <div className='skills__box'>
+            <div className='skills__group'>
+              {backend.map((backend) =>
+                backend.id <= 3 ? (
+                  <div className='skills__data' key={backend.id}>
+                    <i className='bx bx-badge-check'></i>
+
+                    <div>
+                      <h3 className='skills__name'>{backend.skillName}</h3>
+                      <span className='skills_level'>{backend.level}</span>
+                    </div>
+                  </div>
+                ) : null
+              )}
+            </div>
+
+            <div className='skills__group'>
+              {backend.map((backend) =>
+                backend.id >= 4 ? (
+                  <div className='skills__data' key={backend.id}>
+                    <i className='bx bx-badge-check'></i>
+
+                    <div>
+                      <h3 className='skills__name'>{backend.skillName}</h3>
+                      <span className='skills_level'>{backend.level}</span>
+                    </div>
+                  </div>
+                ) : null
+              )}
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
